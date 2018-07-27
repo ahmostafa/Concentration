d7035a522a207c375af83afa2575f5f5e8373c73
Ahmed Mostafa <engahmed.mostafa@yahoo.com>
2018-07-28 02:29:47 +0300

    second week full with random cards

--- Concentration/Concentration.swift
+++ Concentration/Concentration.swift
@@ -11,14 +11,35 @@ import Foundation
 class Concentration{
     
     var cards = [Card]()
-    
+    var indexOfOneAndOnlyFacedCard:Int? //optional
     func chooseCard(at index:Int){
-        if cards[index].isFacedUp{
-            cards[index].isFacedUp = false
+//        if cards[index].isFacedUp{
+//            cards[index].isFacedUp = false
+//        }
+//        else{
+//            cards[index].isFacedUp = true
+//        }
+        
+        if !cards[index].isMatched{
+            
+            if let matchedIndex = indexOfOneAndOnlyFacedCard ,  matchedIndex != index{
+                // check if cards matched
+                if cards[matchedIndex].identifire == cards[index].identifire{
+                    cards[matchedIndex].isMatched=true
+                    cards[index].isMatched=true
+                }
+                cards[index].isFacedUp=true
+                indexOfOneAndOnlyFacedCard=nil
+            }else{
+                //either two cards or one card are matched
+                for flipDownIndex in cards.indices{
+                    cards[flipDownIndex].isFacedUp=false
+                }
+                cards[index].isFacedUp=true
+                indexOfOneAndOnlyFacedCard = index
+            }
         }
-        else{
-            cards[index].isFacedUp = true
-        }
+        
     }
     
     init(numberOfPairsOfCards: Int) {
@@ -26,6 +47,12 @@ class Concentration{
             let card = Card()
             cards += [card,card]
         }
+        
+        for index in cards.indices{
+            let randomIndex = Int (arc4random_uniform(UInt32(cards.count)))
+            cards.swapAt(index, randomIndex)
+        }
+        
         //TODO: Shuffle the cards 
     }
 }
